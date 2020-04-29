907fc98004b31802f710e7b10e34b082d47451c2
niallproche <niall.roche@mishcon.com>
2020-04-29 13:50:39 +0100

    testing argument sending with hardcoded test variables

--- computation-archive/src/url-requests.py
+++ computation-archive/src/url-requests.py
@@ -4,6 +4,14 @@ import ast
 import requests
 import os
 
+"""
+The Smart Contract caller can pass in data to be processed, this can be urls to fetch and required headers and parameters
+
+additional named parameters for post processing could be specified too to make this code more generic 
+These could be a set of regular expressions or parse rules to post-process the data 
+need to be careful that any of the params passed will not be an issue for the requests lib or for the url called
+"""
+
 # parse env args
 arg = [os.environ['ARG0'], os.environ['ARG1']]
 
@@ -13,6 +21,8 @@ else: kwargs = {}
 
 # attempt the request
 req = requests.request(arg[0], arg[1], **kwargs)
+
+# check if post processor params were included
 
 # print text result on single line
 print(req.text.replace('\n',''))
