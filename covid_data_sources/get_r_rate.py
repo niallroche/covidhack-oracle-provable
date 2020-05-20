497785f2ec35c177012a7cddc30e59cf41380d2e
Walter Hernandez Cruz <walter.hernandezcruz@mishcon.com>
2020-05-20 17:51:40 +0100

    Added script to get r rate from London School of Hygiene and Tropical Medicine source

---
+++ covid_data_sources/get_r_rate.py
@@ -0,0 +1,35 @@
+import requests as requests
+from lxml import html
+import re
+
+def get_content(*args, **kwargs):
+
+    r_rate_data = {}
+    response = requests.get('https://epiforecasts.io/covid/posts/national/united-kingdom/')
+
+    tree = html.fromstring(response.content)
+    table_content = tree.xpath('//div[@data-layout="l-body-outset"]/table//tr')
+
+    regions = []
+    for value in table_content:
+        value = value.xpath('.//td//text()')
+
+        if len(value) != 0:
+            r_rate = [float(s) for s in re.findall(r'[+-]?(\d+(?:\.\d+)?)',value[3].strip())]
+            regions_dict = {"region": value[0].strip(),
+                            "effective_reproductive_number_(r_rate)": {"r_rate": r_rate[0],
+                                                                       "lower_bound": r_rate[1],
+                                                                       "upper_bound": r_rate[2]}
+                            }
+
+            regions.append(regions_dict)
+
+    r_rate_data['regions'] = regions
+
+    return r_rate_data
+
+
+
+
+
+
