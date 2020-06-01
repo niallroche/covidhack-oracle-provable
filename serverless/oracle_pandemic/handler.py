ce40b39317ff5f2a1f51cbb45d0c1c06978d6c51
niallproche <niall.roche@mishcon.com>
2020-06-01 19:28:59 +0100

    modified serverless framework to use docker to avoid issues with lxml

--- serverless/oracle_pandemic/handler.py
+++ serverless/oracle_pandemic/handler.py
@@ -1,5 +1,10 @@
 import json
 
+# from covid_data_sources.get_coronavirus_data_gov_uk import get_content
+from covid_data_sources import get_coronavirus_data_gov_uk
+from covid_data_sources import get_r_rate
+from covid_data_sources import get_business_types_regulated
+
 def check_pandemic(event, context):
     body = is_pandemic_in_force(event)
 
@@ -31,6 +36,18 @@ def get_uk_alert_level(event, context):
 
     return response
 
+def get_uk_infection_rate_level(event, context):
+    # check for regional specific queries
+    body = get_corona_infection_rate_uk(event, context)
+    # body = {"error": "coronadata content changed"}
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
 def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
@@ -52,12 +69,13 @@ def hello(event, context):
         "event": event
     }
     """
+
+
 # Oracle records that there is a pandemic in [England]
-def is_pandemic_in_force(query_attributes):
+def is_pandemic_in_force(query_attributes, *args, **kwargs):
     # check query_attributes for nation specifics (e.g. Scotland and NI may have different restrictions and status)
 
     structured_text = {
-        "force_majeure": True,
         "event_type": "pandemic",
         "event_name": "COVID-19",
         "event_designation_date": "2020-03-11T00:16:26-00:00", # WHO press release and twitter 4:26 PM GMT
@@ -67,28 +85,102 @@ def is_pandemic_in_force(query_attributes):
         "resulting_action_source": "government_UK",
         "action_type": "business_closure",
         "date_of_closure": "2020-03-23T00:00:00-00:00",
+        "regions_affected": ["England"],
         # "business_type": ["hairdressers", "barbers", "beauty and nail salons, including piercing and tattoo parlours'"]
-      }
+    }
+    # structured_text["business_type"] = get_business_types_regulated.get_content(*args, **kwargs)
+    structured_text["business_type"] = business_closures
     return structured_text
 
+
 def get_corona_cases_uk(event, context, *args, **kwargs):
-    corona_cases_uk = {
-        "Total number of lab-confirmed UK cases": 246406,
-        "Daily number of lab-confirmed UK cases": 2684,
-        "Total number of COVID-19 associated UK deaths": 34796,
-        "Daily number of COVID-19 associated UK deaths": 160,
+    return get_coronavirus_data_gov_uk.get_content(*args, **kwargs)
 
-        "nations": [
-            {"nation": "England", "total_cases": 144127, "rate": 257.5, "total_deaths": 31010},
-            {"nation": "Northern Ireland", "total_cases": 4401, "rate": 233.9, "total_deaths": 476},
-            {"nation": "Scotland", "total_cases": 14594, "rate": 268.4, "total_deaths": 2103},
-            {"nation": "Wales", "total_cases": 12404, "rate": 395.2, "total_deaths": 1207}
-        ],
 
-        "regions": [],
+def get_corona_infection_rate_uk(event, context, *args, **kwargs):
+    return get_r_rate.get_content(*args, **kwargs)
 
-        "UTLA": [{"region": "Worcestershire", "total_cases": 1334, "rate": 225.3}],
-
-        "LTLA": [{"region": "Malvern Hills", "total_cases": 137, "rate": 175.4}]
-    }
-    return corona_cases_uk
+business_closures = {
+    "business_closures": [
+        "Workplace canteens may remain open where there is no practical alternative for staff at that workplace to obtain food",
+        "Public houses",
+        "Cinemas",
+        "Theatres",
+        "Nightclubs",
+        "Bingo halls",
+        "Concert halls",
+        "Museums and galleries",
+        "Casinos",
+        "Betting shops",
+        "Spas",
+        "Massage parlours",
+        "Tattoo and piercing parlours",
+        "Skating rinks",
+        "Funfairs (whether outdoors or indoors)",
+        "Outdoor markets",
+        "Car showrooms",
+        "Auction houses",
+        "Restaurants",
+        "Restaurants and dining rooms in hotels or members' clubs",
+        "Bars",
+        "Bars in hotels or members' clubs",
+        "Nail",
+        "Beauty",
+        "Hair salons and barbers",
+        "Indoor fitness studios",
+        "Gyms",
+        "Swimming pools",
+        "Bowling alleys",
+        "Amusement arcades or soft play areas or other indoor leisure centres or facilities",
+        "Playgrounds",
+        "Sports courts",
+        "Cafes",
+        "Workplace canteens"
+    ],
+    "business_exceptions": [
+        "Off licenses and licensed shops selling alcohol ( breweries)",
+        "Pharmacies ( non-dispensing pharmacies) and chemists",
+        "Newsagents",
+        "Petrol stations",
+        "Car repair and mot services",
+        "Bicycle shops",
+        "Short term loan providers",
+        "Post offices",
+        "Funeral directors",
+        "Laundrettes and dry cleaners",
+        "Veterinary surgeons and pet shops",
+        "Agricultural supplies shop",
+        "Car parks",
+        "Public toilets",
+        "Garden centres",
+        "Outdoor sports courts",
+        "Food retailers",
+        "Food markets",
+        "Supermarkets",
+        "Convenience stores and corner shops",
+        "Homeware",
+        "Building supplies and hardware stores",
+        "Banks",
+        "Credit unions",
+        "Savings clubs",
+        "Undertakings which by way of business operate currency exchange offices",
+        "Dental services",
+        "Opticians",
+        "Audiology services",
+        "Chiropody",
+        "Chiropractors",
+        "Services relating to mental health",
+        "Storage and distribution facilities",
+        "Where the facilities are in the premises of a business included in this part",
+        "Building societies",
+        "Cash points",
+        "Taxi",
+        "Vehicle hire businesses",
+        "Transmit money (or any representation of money) by any means",
+        "Cash cheques which are made payable to customers",
+        "Osteopaths and other medical",
+        "Health services",
+        "Delivery drop off",
+        "Collection points"
+    ]
+}
